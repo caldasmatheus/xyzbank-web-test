ffced2e792c46c8fa3324599360abceac2a22de7
Matheus Caldas <raimundo_caldas@terceiros.sicredi.com.br>
2024-09-30 21:00:59 -0300

    :recycle: feat:add ajuste na basePage para que o chrome execute em modo headless e os testes executem na actions

--- pages/basePage.py
+++ pages/basePage.py
@@ -1,20 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 class Base:
-    def __init__(self, driver, browser=None):
-        if driver:
-            self.driver = driver
-        else:
+    def __init__(self, driver=None, browser='chrome'):
+        if driver is None:
             if browser == 'chrome':
-                self.driver = webdriver.Chrome()
-            elif browser == 'safari':
-                self.driver = webdriver.Safari()
-            elif browser == 'firefox':
-                self.driver = webdriver.Firefox()
-            else:
-                raise Exception('Browser não suportado!')
+                chrome_options = Options()
+                chrome_options.add_argument('--headless')
+                chrome_options.add_argument('--no-sandbox')
+                chrome_options.add_argument('--disable-dev-shm-usage')
+                self.driver = webdriver.Chrome(options=chrome_options)
+        else:
+            self.driver = driver
 
     def is_url(self, url):
         return self.driver.current_url == url
