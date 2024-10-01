4a1e7e62c2c017050fb112981124757efab3837d
AlesandraIsla <alesandramempis@gmail.com>
2024-09-30 22:19:31 -0300

    Teste 3

--- pages/customerPage.py
+++ pages/customerPage.py
@@ -14,6 +14,9 @@ class CustomerPage(Base):
     MENU_DEPOSIT_BTN = "//button[contains(text(), 'Deposit')]"
     MENU_WITHDRAWL_BTN = "//button[contains(text(), 'Withdrawl')]"
 
+    MENU_TRANSACTIONS_BTN_RESET = "//button[contains(text(), 'Reset')]"
+
+
     AMOUNT = "//input[@placeholder='amount']"
     DEPOSIT_BTN = "//form[contains(@name, 'myForm')]//button[contains(text(), 'Deposit')]"
 
@@ -115,3 +118,24 @@ class CustomerPage(Base):
             EC.presence_of_element_located((By.CSS_SELECTOR, self.DELETE_BTN))
         )
         self.wait_element((By.CSS_SELECTOR, self.DELETE_BTN)).click()
+
+    def click_transactions_tab(self):
+        transactions_tab = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Transactions')]"))
+        )
+        transactions_tab.click()
+
+    def get_transaction_rows(self):
+        transactions_table = WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//table[@class='table table-bordered table-striped']/tbody"))
+        )
+        transaction_rows = transactions_table.find_elements(By.TAG_NAME, 'tr')
+        return transaction_rows
+
+    def click_reset_button(self):
+        self.wait_element((By.XPATH, self.MENU_TRANSACTIONS_BTN_RESET)).click()
+
+    def verify_no_transactions(self):
+        transaction_rows = self.get_transaction_rows()
+        return len(transaction_rows) == 0
+
